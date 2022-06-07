f47e1c6867b66a727a9b8d2ee722d1caadf8e3cd
erianvc <erianvc.dev@gmail.com>
2022-06-07 11:47:45 -0500

    refactor: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import path from 'path'
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@store': path.resolve(__dirname, 'src/store'),
-      '@views': path.resolve(__dirname, 'src/views'),
+      '@components': path.resolve(dirname, 'src/components'),
+      '@routes': path.resolve(dirname, 'src/routes'),
+      '@utils': path.resolve(dirname, 'src/utils'),
+      '@store': path.resolve(dirname, 'src/store'),
+      '@views': path.resolve(dirname, 'src/views'),
     },
   },
   server: {
